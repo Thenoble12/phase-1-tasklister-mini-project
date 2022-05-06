06e43017e8f3e29b04aff25483bc6d84421ffd4b
Elijah Noble <elijahnobledev@gmail.com>
2022-05-05 17:20:59 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildTasklist(e.target["new-task-description"].value)
+  })
 });
+
+function buildTasklist(task){
+  let item = document.createElement('li') // createing li element and setting it to item
+  let btn = document.createElement("button") // createing button element and setting it to btn
+  btn.addEventListener('click', handleDelete)  
+  btn.textContent = 'x'
+  item.textContent = `${task} `
+  item.appendChild(btn)
+  console.log(item)
+  document.querySelector('#list').appendChild(item)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
